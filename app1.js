20efb66c9522f34e41310512aba7295f00b7422e
Igor <igorabeloaz@gmail.com>
2024-03-28 15:20:51 -0300

    subir arquivos iniciais

---
+++ app1.js
@@ -0,0 +1,14 @@
+
+//console.log('nomes');
+
+//let pessoas=require('./dados')
+//console.log(pessoas);
+//console.log(pessoas[2]);
+
+//console.log(pessoas.nomes);
+//console.log(pessoas.frutas);
+
+let{nomes, frutas,Soma}=require('./dados');
+console.log(nomes);
+console.log(frutas);
+console.log(Soma(2,4));
